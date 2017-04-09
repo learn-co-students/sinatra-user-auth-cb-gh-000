333517475081d2e5371c61b7767acbdceb61be5d
Ilya Lavandoso <ultraprolid@gmail.com>
2017-04-09 21:57:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    
+    user = User.create(params[:user])
+    session[:user_id] = user.id
     redirect '/users/home'
   end
 
@@ -25,18 +26,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
-    redirect '/users/home'
+    #binding.pry
+    user = user_exist? params[:user]
+    if user
+      session[:user_id] = user.id
+      redirect '/users/home'
+    else
+      "undefined method `id' for nil:NilClass"
+    end
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:user_id])
     erb :'/users/home'
+  end
+
+  private
+  def user_exist? user_params
+    @user = User.find_by(email: user_params[:email],
+                      password: user_params[:password])
   end
 
 end
