85d21cc2c76e909aadc02827a187d5168f90644d
Hayden Betts <haydenbetts@gmail.com>
2017-09-21 14:03:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    #at this point @user variable displays correctly
+    @user.save
+    # at this point @user variable also displays correctly...
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -31,17 +35,26 @@ class ApplicationController < Sinatra::Base
   # If yes, store user ID as the value of session[:id]
   post '/sessions' do
 
+    # underneath this is a SQL and query, for which both parameters must be
+    # satisified.
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
   # clear the session hash
   get '/sessions/logout' do
-
+    session.clear
     redirect '/'
   end
 
   # render the users homepage view
   get '/users/home' do
+
+    # right, so the way that we pass the user object from one
+    # controller > view to another is by looking it up with session id
+    # which is set on the first page the user logs into
+    @user = User.find(session[:id])
 
     erb :'/users/home'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  # TODO what does this do?
   validates_presence_of :name, :email, :password
+
 end
